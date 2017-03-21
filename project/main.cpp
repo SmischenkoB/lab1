4c7d4af84f986c5929b1fa3a841dbc79249c2142
SmischenkoB <bogdan.smishenko@gmail.com>
2017-03-21 19:55:30 +0200

    base things

---
+++ project/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+struct  file{
+    public:
+char name[100];
+int scores[19];
+};
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
